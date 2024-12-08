446955acf4f63fe51cf151a90e2ad399b4e78db8
Yujing14 <yuyu1@s.sfusd.edu>
2024-12-07 21:16:41 -0800

    Lesson 10 with P1+P2 and Lesson 11

--- l2dfz-0.14.0-win/l2dfz-0.14.0-win/projects/M07.telling_a_story_workbook/lessons/dialogue.gd
+++ l2dfz-0.14.0-win/l2dfz-0.14.0-win/projects/M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -3,14 +3,39 @@ extends Control
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
 @onready var next_button: Button = %NextButton
 @onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
+@onready var body: TextureRect = %Body
+@onready var expression: TextureRect = %Expression
 
-var dialogue_items: Array[String] = [
-	"Hello there!",
-	"The work for me to do is tough......",
-	"Let's make our best effort to reach to sucess!",
-	"I think I got it? Did I?",
-	"Goodbye! See you next time!"
+var expressions := {
+	"happy": preload ("res://assets/emotion_happy.png"),
+	"regular": preload ("res://assets/emotion_regular.png"),
+	"sad": preload ("res://assets/emotion_sad.png"),
+}
+
+
+var dialogue_items: Array[Dictionary] = [
+	{
+		"expression": expressions["regular"],
+		"text": "Hello there!"
+	},
+	{
+		"expression": expressions["sad"],
+		"text": "The work for me to do is tough......"
+	},
+	{
+		"expression": expressions["happy"],
+		"text": "Let's make our best effort to reach to sucess!",
+	},
+	{
+		"expression": expressions["regular"],
+		"text": "I think I got it? Did I?",
+	},
+	{
+		"expression": expressions["happy"],
+		"text": "Goodbye! See you next time!",
+	},
 ]
+
 
 var current_item_index := 0
 
@@ -20,7 +45,8 @@ func _ready() -> void:
 
 func show_text() -> void:
 	var current_item := dialogue_items[current_item_index]
-	rich_text_label.text = current_item
+	rich_text_label.text = current_item["text"]
+	expression.texture = current_item["expression"]
 	rich_text_label.visible_ratio = 0.0
 	var tween := create_tween()
 	var text_appearing_duration := 1.2
@@ -29,7 +55,7 @@ func show_text() -> void:
 	var sound_start_position := randf() * sound_max_length
 	audio_stream_player.play(sound_start_position)
 	tween.finished.connect(audio_stream_player.stop)
-	
+	slide_in()
 
 func advance() -> void:
 	current_item_index += 1
@@ -37,3 +63,12 @@ func advance() -> void:
 		get_tree().quit()
 	else:
 		show_text()
+
+func slide_in() -> void:
+	var tween := create_tween()
+	tween.set_trans(Tween.TRANS_QUART)
+	tween.set_ease(Tween.EASE_OUT)
+	body.position.x = 200.0
+	tween.tween_property(body, "position:x", 0.0, 0.3)
+	body.modulate.a = 0.0
+	tween.parallel().tween_property(body, "modulate:a", 1.0, 0.2)
