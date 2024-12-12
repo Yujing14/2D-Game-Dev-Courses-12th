ce3f59a8dd34c8ba0bec2208873ba1296a890681
Yujing14 <yuyu1@s.sfusd.edu>
2024-12-11 23:04:35 -0800

    Lesson 4

---
+++ l2dfz-0.14.0-win/l2dfz-0.14.0-win/projects/M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -0,0 +1,114 @@
+extends Control
+
+@onready var rich_text_label: RichTextLabel = %RichTextLabel
+@onready var next_button: Button = %NextButton
+@onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
+@onready var body: TextureRect = %Body
+@onready var expression: TextureRect = %Expression
+
+var expressions := {
+	"happy": preload ("res://assets/emotion_happy.png"),
+	"regular": preload ("res://assets/emotion_regular.png"),
+	"sad": preload ("res://assets/emotion_sad.png"),
+}
+
+var bodies := {
+	"sophia": preload ("res://assets/sophia.png"),
+	"pink": preload ("res://assets/pink.png")
+}
+
+var dialogue_items: Array[Dictionary] = [
+	{
+		"expression": expressions["regular"],
+		"text": "Hello [wave] there! [/wave]",
+		"character": bodies["sophia"],
+	},
+	{
+		"expression": expressions["regular"],
+		"text": "Hello! Long time no see!",
+		"character": bodies["pink"],
+	},
+	{
+		"expression": expressions["sad"],
+		"text": "The coding work for me to do lately is [shake] tough[/shake]......",
+		"character": bodies["sophia"],
+	},
+		{
+		"expression": expressions["sad"],
+		"text": "That is a sad news to hear of...",
+		"character": bodies["pink"],
+	},
+	{
+		"expression": expressions["happy"],
+		"text": "But I will do my very BEST[tornado freq=3.0][rainbow val=1.0] to finish all the work![/rainbow][/tornado]",
+		"character": bodies["sophia"],
+	},
+	{
+		"expression": expressions["happy"],
+		"text": "Yes! I believe in you!",
+		"character": bodies["pink"],
+	},
+	{
+		"expression": expressions["regular"],
+		"text": "Now I think I am good with the work now since I finished it.",
+		"character": bodies["sophia"],
+	},
+		{
+		"expression": expressions["regular"],
+		"text": "Glad to hear about that.",
+		"character": bodies["pink"],
+	},
+	{
+		"expression": expressions["happy"],
+		"text": "Goodbye! See you next time!",
+		"character": bodies["sophia"],
+	},
+		{
+		"expression": expressions["happy"],
+		"text": "Bye bye~ See you~",
+		"character": bodies["pink"],
+	},
+]
+
+
+
+var current_item_index := 0
+
+func _ready() -> void:
+	show_text()
+	next_button.pressed.connect(advance)
+
+func show_text() -> void:
+	var current_item := dialogue_items[current_item_index]
+	rich_text_label.text = current_item["text"]
+	expression.texture = current_item["expression"]
+	body.texture = current_item["character"]
+	rich_text_label.visible_ratio = 0.0
+	var tween := create_tween()
+	var text_appearing_duration: float = current_item["text"].length() / 30.0
+	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
+	var sound_max_length := audio_stream_player.stream.get_length() - text_appearing_duration
+	var sound_start_position := randf() * sound_max_length
+	audio_stream_player.play(sound_start_position)
+	tween.finished.connect(audio_stream_player.stop)
+	slide_in()
+	next_button.disabled = true
+	tween.finished.connect(func() -> void:
+		next_button.disabled = false
+	)
+
+func advance() -> void:
+	current_item_index += 1
+	if current_item_index == dialogue_items.size():
+		get_tree().quit()
+	else:
+		show_text()
+
+func slide_in() -> void:
+	var tween := create_tween()
+	tween.set_trans(Tween.TRANS_QUART)
+	tween.set_ease(Tween.EASE_OUT)
+	body.position.x = 200.0
+	tween.tween_property(body, "position:x", 0.0, 0.3)
+	body.modulate.a = 0.0
+	tween.parallel().tween_property(body, "modulate:a", 1.0, 0.2)
--- l2dfz-0.14.0-win/l2dfz-0.14.0-win/projects/M08.start_a_dialog_workbook/lessons/dialogue.gd
+++ l2dfz-0.14.0-win/l2dfz-0.14.0-win/projects/M08.start_a_dialog_workbook/lessons/dialogue.gd
@@ -18,41 +18,38 @@ var bodies := {
 var dialogue_items: Array[Dictionary] = [
 	{
 		"expression": expressions["regular"],
-		"text": "I've been learning about [wave]Arrays and Dictionaries[/wave]",
-		"character": bodies["sophia"]
-	},
-	{
-		"expression": expressions["regular"],
-		"text": "How has it been going?",
-		"character": bodies["pink"]
-	},
-	{
-		"expression": expressions["sad"],
-		"text": "... Well... it is a little bit [shake]complicated[/shake]!",
-		"character": bodies["sophia"]
-	},
-	{
-		"expression": expressions["sad"],
-		"text": "Oh!",
-		"character": bodies["pink"]
-	},
-	{
-		"expression": expressions["regular"],
-		"text": "I believe in you!",
-		"character": bodies["pink"]
+		"text": "[wave]Hey, wake up![/wave]\nIt's time to make video games.",
+		"character": bodies["sophia"],
+		"choices": {
+			"Let me sleep a little longer": 2,
+			"Let's do it!": 1,
+		},
 	},
 	{
 		"expression": expressions["happy"],
-		"text": "If you stick to it, you'll eventually make it!",
-		"character": bodies["pink"]
+		"text": "Great! Your first task will be to write a [b]dialogue tree[/b].",
+		"character": bodies["sophia"],
+		"choices": {
+			"I will do my best": 3,
+			"No, let me go back to sleep": 2,
+		},
+	},
+	{
+		"expression": expressions["sad"],
+		"text": "Oh, come on! It'll be fun.",
+		"character": bodies["pink"],
+		"choices": {
+			"No, really, let me go back to sleep": 0,
+			"Alright, I'll try": 1,
+		},
 	},
 	{
 		"expression": expressions["happy"],
-		"text": "That's it! Let's [tornado freq=3.0][rainbow val=1.0]GOOOOOO!!![/rainbow][/tornado]",
-		"character": bodies["sophia"]
-	}
+		"text": "That's the spirit! [wave]You can do it![/wave]",
+		"character": bodies["pink"],
+		"choices": {"Okay! (Quit)": - 1},
+	},
 ]
-
 
 ## UI element that shows the texts
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
@@ -70,7 +67,6 @@ func _ready() -> void:
 	show_text(0)
 
 
-
 ## Draws the current text to the rich text element
 func show_text(current_item_index: int) -> void:
 	# We retrieve the current item from the array
@@ -81,7 +77,7 @@ func show_text(current_item_index: int) -> void:
 	rich_text_label.text = current_item["text"]
 	expression_texture_rect.texture = current_item["expression"]
 	body.texture = current_item["character"]
-
+	create_buttons(current_item["choices"])
 	# We set the initial visible ratio to the text to 0, so we can change it in the tween
 	rich_text_label.visible_ratio = 0.0
 	# We create a tween that will draw the text
@@ -105,9 +101,13 @@ func show_text(current_item_index: int) -> void:
 
 	# We animate the character sliding in.
 	slide_in()
-
 	# Finally, we disable the next button until the text finishes displaying.
-
+	for button: Button in action_buttons_v_box_container.get_children():
+		button.disabled = true
+	tween.finished.connect(func() -> void:
+		for button: Button in action_buttons_v_box_container.get_children():
+			button.disabled = false
+	)
 
 ## Progresses to the next slide.
 
@@ -119,3 +119,20 @@ func slide_in() -> void:
 	slide_tween.tween_property(body, "position:x", 0, 0.3)
 	body.modulate.a = 0
 	slide_tween.parallel().tween_property(body, "modulate:a", 1, 0.2)
+
+func create_buttons(choices_data: Dictionary) -> void:
+	for button in action_buttons_v_box_container.get_children():
+		button.queue_free()
+	# We loop over all the dictionary keys
+	for choice_text in choices_data:
+		var button := Button.new()
+		button.text = choice_text
+		action_buttons_v_box_container.add_child(button)
+		var target_line_idx: int = choices_data[choice_text]
+		if target_line_idx == - 1:
+			# If the target line index is -1, we want to quit
+			button.pressed.connect(get_tree().quit)
+		else:
+			# Otherwise we bind the target line index to the show_text function
+			# and use that in the pressed signal's connection
+			button.pressed.connect(show_text.bind(target_line_idx))
